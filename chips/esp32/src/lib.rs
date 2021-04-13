86e5b09b7e811d57246900c263bd1a5ed9326cb5
Alistair Francis <alistair.francis@wdc.com>
2021-04-13 11:44:46 +1000

    chips/esp32: Initial commit of ESP32 UART
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

---
+++ chips/esp32/src/lib.rs
@@ -0,0 +1,8 @@
+//! Drivers and chip support for Espressif ESP32 boards.
+
+#![feature(asm, const_fn_trait_bound, naked_functions)]
+#![no_std]
+#![crate_name = "esp32"]
+#![crate_type = "rlib"]
+
+pub mod uart;
